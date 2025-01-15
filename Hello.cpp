7434823ea7e9752566ccc63fb600f19ec11c034a
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-01-15 18:47:48 +0000

    Hello World first test

---
+++ Hello.cpp
@@ -0,0 +1,18 @@
+//CSC134
+//Hello World
+// Gabriel Cunningham
+//1/15/25
+
+//#include lets use input and output
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout <<"Hello, World!" << endl;
+    cout <<"This is a test" << endl;
+    cout <<"This is showing another way to end a line of code \n";
+    return 0;
+    /* To my current knowledge cout will allow you to print out text.*/
+}
