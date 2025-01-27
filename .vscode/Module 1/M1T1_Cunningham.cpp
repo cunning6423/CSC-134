afb6a14aac1be8f83aec5c30572419548bc9eca4
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-01-27 18:31:34 +0000

    M1T1_Cunningham_HelloWorld

---
+++ .vscode/Module 1/M1T1_Cunningham.cpp
@@ -0,0 +1,15 @@
+//CSC 134
+//M1T1 - Hello World
+//Cunningham
+//1/27/2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Hello" << endl;
+    cout <<endl;
+    return 0; // no errors
+
+}
