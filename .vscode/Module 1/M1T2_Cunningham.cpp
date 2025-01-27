b07382c542de9ec63977c8adbd04233683c8247e
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-01-27 18:42:16 +0000

    M1T2 Tried

---
+++ .vscode/Module 1/M1T2_Cunningham.cpp
@@ -0,0 +1,16 @@
+//CSC 134
+//M1T2 - Playing with code
+//Cunningham
+//1/27/2025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Hello World" << endl;
+    cout <<endl; // no errors
+    cout <<"Gabriel Cunningham" << endl;
+    cout <<endl;
+    return 0;
+}
