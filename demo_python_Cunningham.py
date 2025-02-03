fa29a3df8556101db4b938f4ef6369280afe2750
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-02-03 14:29:44 -0500

    Add files via upload
    Here is this cool python.

---
+++ demo_python_Cunningham.py
@@ -0,0 +1,18 @@
+from turtle import*
+
+t= Turtle()
+t.pensize(3)
+t.pencolor("blue")
+# square - 90 degrees * 4 is 360
+#The comment system for python
+
+t.begin_fill()  #fill this shape
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.end_fill()    #Finish filling
+
