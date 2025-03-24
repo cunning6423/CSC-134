a0fda7c9fb87fc8e5952323327aee34a3ec90107
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-03-24 18:31:32 +0000

    Gambling centeral incomplete

--- .vscode/Code_Example/functionFun.cpp
+++ .vscode/Code_Example/functionFun.cpp
@@ -11,19 +11,30 @@ int bankAccount(int balance, int withDraw, int deposit);
 
 int main(){
     cout << " Welcome to Bank Safety!";
+    int balance, withDraw, deposit;
     bankAccount(balance, withDraw, deposit);
+    cout <<"Your balance: $" << balance << endl;
 }
+
+
 int bankAccount(int balance, int withDraw, int deposit){
     string withdrawChoice;
     string depositChoice;
-    cout <<"Enter balance: ";
+    cout <<"\nEnter balance: $";
     cin >> balance;
     cout <<"Do you want to deposit (Yes or No): ";
-    cin depositChoice;
+    cin >> depositChoice;
     if (depositChoice == "yes"){
-        cout << "Deposit amount: " ;
-        cin >> deposit
+        cout << "Deposit amount: $" ;
+        cin >> deposit;
         balance = balance + deposit;
     }
-    
+    cout <<"Do you want to withdraw (Yes or No): ";
+    cin >> withdrawChoice;
+    if (withdrawChoice == "Yes"){
+        cout << "Withdraw amount: $" ;
+        cin >> withDraw;
+        balance = balance - withDraw;
+    }
+    return balance;
 }
