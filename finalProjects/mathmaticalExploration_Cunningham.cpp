7f54a4b4b3012782336bd21dd4cd254504587c4c
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-04-28 18:41:04 +0000

    Incomplete Final Project

--- finalProjects/mathmaticalExploration_Cunningham.cpp
+++ finalProjects/mathmaticalExploration_Cunningham.cpp
@@ -10,25 +10,25 @@ Explore the world free, however the only thing that blocks you way MATH!!!
 #include <ctime>
 using namespace std;
 
-int generateMathQuestion();
+int generateMathQuestion(int choice, int level, int aD, int mD, int sD, int dD);
 void printDirectionChoices();
-bool gameRound(int level);
+bool gameRound(int aD, int mD, int sD, int dD);
 string mainMenu();
 
 int main()
 {
     srand(time(0));  // Initialize random seed
     int level = 1;  // Game starts at level 1
-    int additionDifficulty = 1;
-    int subtractionDifficulty = 1;
-    int multiplicationDifficulty = 1;
-    int divisionDifficulty = 1;
+    int aD = 1; //additionDifficulty
+    int sD = 1; //subtractionDifficulty
+    int mD = 1; //multiplicationDifficulty
+    int dD = 1; //divisionDifficulty
     cout << "Welcome to the Walking Upstairs Game!\n";
         while (true) {
             cout << "\nLevel " << level << ":\n";
     
             // Run one round of the game
-            if (!gameRound(level)) {
+            if (!gameRound(aD, mD, sD, dD)) {
                 break; // If the player gets the question wrong, end the game
             }
     
@@ -45,57 +45,38 @@ int main()
 }
 
 string mainMenu(){
-    string choice;
-    cout <<"\nEnter Adventure mode\nEnter Study Mode\nQuit"<< endl;
-    cout <<"(Enter with Cap letters)" << endl;
-    cin>> choice;
-    if (choice == "Adventure" || choice == "A" || choice == "Adventure Mode")
-    {
-        entranceHall();
-    }
-    else if (choice == "Study " || choice == "S" || choice == "Study Mode"){
-        gameRound();
-    }
-    else if (choice == "Quit" || choice == "Q"){
-        main();
-    }
-    else{
-        cout << "Invalid Option... Try Again";
-        mainMenu();
-    }
-    return choice;
+return 0;
 }
 
-int generateMathQuestion(int level) {
-    int answer = a + b;
-
+int generateMathQuestion(int choice, int aD, int sD, int mD, int dD) {
+    int answer, a,b;
     //When the direction is pick a certain math equation is choosen
     switch (choice) {
         case 1: // Addition
-            a = rand() % (additionDifficulty * 10);
-            b = rand() % (additionDifficulty * 10);
+            a = rand() % (aD * 10);
+            b = rand() % (aD * 10);
             answer = a + b;
             cout << "What is " << a << " + " << b << "? ";
             break;
         case 2: // Multiplication
-            a = rand() % (multiplicationDifficulty * 10);
-            b = rand() % (multiplicationDifficulty * 10);
+            a = rand() % (mD * 10);
+            b = rand() % (mD * 10);
             answer = a * b;
             cout << "What is " << a << " * " << b << "? ";
             break;
         case 3: // Subtraction
-            a = rand() % (subtractionDifficulty * 10);
-            b = rand() % (subtractionDifficulty * 10);
+            a = rand() % (sD * 10);
+            b = rand() % (sD * 10);
             answer = (a > b) ? a - b : b - a;
             cout << "What is " << max(a, b) << " - " << min(a, b) << "? ";
             break;
         case 4: // Division
-            a = rand() % (divisionDifficulty * 10) + 1;  // Avoid division by zero
-            b = rand() % (divisionDifficulty * 5) + 1;  
+            a = rand() % (dD * 10) + 1;  // Avoid division by zero
+            b = rand() % (dD * 5) + 1;  
             answer = a / b;
             cout << "What is " << a << " / " << b << "? (Rounded to nearest whole number) ";
             break;
-        default{
+        default : {
             break;
         }
     }
@@ -113,36 +94,50 @@ void printDirectionChoices() {
     cout << "5. Quit\n";
 }
 
-bool gameRound(int level) {
+bool gameRound(int aD, int mD, int sD, int dD) {
     // Show direction choices
     printDirectionChoices();
-
-    // Get player's direction choice
     int choice;
+    // Get player's direction choice
     cout <<"Enter 5 to exit" << endl;
-    cout << "Enter your choice (1-4): ";
+    cout << "Enter your choice (1-5): ";
     cin >> choice;
-    if (choice <0 || choice > 5){
-        cout << "Invalid Option... Try Again" << endl;
-        printDirectionChoices();
+    switch(choice) {
+        case 1: // North (Addition)
+            aD++;
+            break;
+        case 2: // East (Multiplication)
+            mD++;
+            break;
+        case 3: // South (Subtraction)
+            sD++;
+            break;
+        case 4: // West (Division)
+            dD++;
+            break;
+        case 5: // Quit Screen
+            break;
     }
-    else if(choice == 5){
-       
+    if(!choice == 5) {
+            // Generate math question for this round
+        int correctAnswer = generateMathQuestion(choice, aD, mD, sD, dD);
+
+        // Get player's answer to the math question
+        int playerAnswer;
+        cout << "Your answer: ";
+        cin >> playerAnswer;
+
+        // Check if the answer is correct
+        if (playerAnswer != correctAnswer) {
+            cout << "\nIncorrect! The correct answer was " << correctAnswer << ".\n";
+            return false;
+        }
+
+        cout << "\nCorrect! You move " << (choice == 1 ? "North" : (choice == 2 ? "East" : (choice == 3 ? "South" : "West"))) << ".\n";
+        return true;
     }
-    // Generate math question for this round
-    int correctAnswer = generateMathQuestion(level);
-
-    // Get player's answer to the math question
-    int playerAnswer;
-    cout << "Your answer: ";
-    cin >> playerAnswer;
-
-    // Check if the answer is correct
-    if (playerAnswer != correctAnswer) {
-        cout << "\nIncorrect! The correct answer was " << correctAnswer << ".\n";
-        return false;
+    else if (choice == 5){
+        cout <<"You have quit!\n" << endl;
     }
-
-    cout << "\nCorrect! You move " << (choice == 1 ? "North" : (choice == 2 ? "East" : (choice == 3 ? "South" : "West"))) << ".\n";
-    return true;
+    
 }
