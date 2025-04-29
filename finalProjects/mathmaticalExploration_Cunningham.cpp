53ae23b08184b8643ff6f161e1e2be93d9a6c995
cunning6423 <cunningg4263@student.faytechcc.edu>
2025-04-29 03:02:16 +0000

    updated

--- finalProjects/mathmaticalExploration_Cunningham.cpp
+++ finalProjects/mathmaticalExploration_Cunningham.cpp
@@ -8,17 +8,22 @@ Explore the world free, however the only thing that blocks you way MATH!!!
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <thread>
+#include <chrono>
 using namespace std;
 
-int generateMathQuestion(int choice, int level, int aD, int mD, int sD, int dD);
-void printDirectionChoices();
-bool gameRound(int aD, int mD, int sD, int dD);
+void printMilestone(int level); // displays checkmarks like level 10, 20, 30 
+int generateMathQuestion(int choice, int level, int aD, int mD, int sD, int dD); // Generates math question when called
+void printDirectionChoices(); // Display the list of actions
+bool gameRound(int choice, int aD, int mD, int sD, int dD); // each round of the game.
 string mainMenu();
+
 
 int main()
 {
     srand(time(0));  // Initialize random seed
     int level = 1;  // Game starts at level 1
+    int choice; // the direction input variable
     int aD = 1; //additionDifficulty
     int sD = 1; //subtractionDifficulty
     int mD = 1; //multiplicationDifficulty
@@ -28,20 +33,22 @@ int main()
             cout << "\nLevel " << level << ":\n";
     
             // Run one round of the game
-            if (!gameRound(aD, mD, sD, dD)) {
+            if (!gameRound(choice, aD, mD, sD, dD)) {
                 break; // If the player gets the question wrong, end the game
             }
     
             // Increase difficulty after each level
             level++;
-    
-            // Optionally, add a delay or some kind of visual cue that they're advancing
-            cout << "You've moved to the next area!\n";
+            cout << "You've moved to the next to the next floor!\n";
         }
     
-    cout << "You made it to level " << level - 1 << ".\n***GAME OVER***" ;
+    cout << "You made it to floor " << level - 1 << ".\n***GAME OVER***" ;
     return 0;
 
+}
+
+void printMilestone(){
+    
 }
 
 string mainMenu(){
@@ -94,12 +101,11 @@ void printDirectionChoices() {
     cout << "5. Quit\n";
 }
 
-bool gameRound(int aD, int mD, int sD, int dD) {
+bool gameRound(int choice, int aD, int mD, int sD, int dD) {
     // Show direction choices
     printDirectionChoices();
-    int choice;
+    
     // Get player's direction choice
-    cout <<"Enter 5 to exit" << endl;
     cout << "Enter your choice (1-5): ";
     cin >> choice;
     switch(choice) {
@@ -118,7 +124,7 @@ bool gameRound(int aD, int mD, int sD, int dD) {
         case 5: // Quit Screen
             break;
     }
-    if(!choice == 5) {
+    if(choice != 5) {
             // Generate math question for this round
         int correctAnswer = generateMathQuestion(choice, aD, mD, sD, dD);
 
@@ -132,12 +138,10 @@ bool gameRound(int aD, int mD, int sD, int dD) {
             cout << "\nIncorrect! The correct answer was " << correctAnswer << ".\n";
             return false;
         }
-
         cout << "\nCorrect! You move " << (choice == 1 ? "North" : (choice == 2 ? "East" : (choice == 3 ? "South" : "West"))) << ".\n";
         return true;
     }
     else if (choice == 5){
         cout <<"You have quit!\n" << endl;
     }
-    
 }
